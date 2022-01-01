2f724a7325d79596bf82956c014c360dbff2284e
Inhwa Cho <ihnhwa8362@naver.com>
2022-01-01 22:58:16 +0900

    first commit

---
+++ practice1.py
@@ -0,0 +1,21 @@
+# 애완동물을 소개해 주세요~
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+'''이렇게
+하면
+여러 문장이
+주석처리
+됩니다
+'''
+
+print("우리집 " + animal + "의 이름은 " + name + "예요")
+hobby = "공놀이"
+#print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# 컨트롤 + / 하면 전체주석
---
+++ practice4-3.py
@@ -0,0 +1,20 @@
+python = "Python is Amazing"
+print(python.lower()) # 모든 문자가 소문자로 됨
+print(python.upper()) # 모든 문자가 대문자로 됨
+print(python[0].isupper()) # 0번쨰 문자가 대문자인가?
+print(len(python)) # 문자열의 길이
+print(python.replace("Python", "Java"))
+# 문자열 python에서 "Python"을 찾아서 "Java"로 대체
+
+index = python.index("n") # 문자열 python에서 "n"의 위치?
+print(index)
+index = python.index("n", index + 1)
+# 두 번째 변수는 스타트 위치, 즉 line 8에서 찾은 "n" 이후의 "n"의 위치?
+print(index)
+
+print(python.find("n"))
+print(python.find("Java"))
+print(python.index("Java"))
+# find는 찾고자 하는 문자열이 없으면 -1을 출력하고 코드가 진행되지만 index는 에러가 발생한다.
+
+print()
